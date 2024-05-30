2416a84cd56b0607dfa7fe4c90957d886b76f785
Konstantin Kulikov <rasmadeus@gmail.com>
2024-05-30 15:49:07 +0200

    Add MySql User Model

---
+++ src/main/java/com/example/rspring/UserController.java
@@ -0,0 +1,30 @@
+package com.example.rspring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+        var user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
